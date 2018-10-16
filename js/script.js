d736b97fff557facbefff2b7364b807c4a84202f
Jeff Liknes <jeff.liknes@gmail.com>
2018-10-16 16:17:29 -0700

    reorganized fonts

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// smooth scroll
+
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: true,
+    prevNextButtons: false,
+  });
+
+  $('.form-submit').on('submit', function(event) {
+    event.preventDefault();
+    if ($('#email').val() == '') {
+      alert('Please submit a valid email address');
+    } else {
+      alert('Thank you for subscribing!');
+    }
+  });
